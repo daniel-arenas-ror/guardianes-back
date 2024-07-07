61728efae203c7fecf9c98ab58042cdcaa0b846c
Daniel Arenas <daniel1144151@gmail.com>
2024-07-07 00:08:18 -0500

    search turn

--- app/controllers/api/v1/turn_controller.rb
+++ app/controllers/api/v1/turn_controller.rb
@@ -2,6 +2,16 @@ module Api
   module V1
     class TurnController < BaseController
       def index
+        turns = Turn
+                  .joins(:service_deal)
+                  .where(
+                    week: params[:week],
+                    service_deal: {
+                      service_id: params[:service_id]
+                    }
+                  )
+
+        render json: turns
       end
     end
   end
