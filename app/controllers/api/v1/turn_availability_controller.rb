7700a55621a94ff34bef3bf3fe9e6af1304f97e4
Daniel Arenas <daniel1144151@gmail.com>
2024-07-06 23:36:31 -0500

    wip availability

--- app/controllers/api/v1/turn_availability_controller.rb
+++ app/controllers/api/v1/turn_availability_controller.rb
@@ -2,12 +2,33 @@ module Api
   module V1
     class TurnAvailabilityController < ActionController::Base
       def index
+        turn_availabilities = TurnAvailability.all
+
+        render json: turn_availabilities
       end
 
       def create
+        turn_availabilities = TurnAvailability.new(turn_availability_params)
+
+        if turn_availabilities.create
+          render json: turn_availabilities, status: :ok
+        else
+          render json: turn_availabilities.errors.full_messages, status: status
+        end
       end
 
       def destroy
+
+        head :ok
+      end
+
+      private
+
+      def turn_availability_params
+        params.required(:turn_availability).permit(
+          :turn_id,
+          :worker_id
+        )
       end
     end
   end
---
+++ app/controllers/api/v1/turn_controller.rb
@@ -0,0 +1,8 @@
+module Api
+  module V1
+    class TurnController < ActionController::Base
+      def index
+      end
+    end
+  end
+end
