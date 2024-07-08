c35768b8a657e6d610da6477f1d33f6b4f0a410f
Daniel Arenas <daniel1144151@gmail.com>
2024-07-07 20:17:49 -0500

    fix cors request

--- Gemfile
+++ Gemfile
@@ -78,3 +78,4 @@ group :test do
 end
 
 gem 'activeadmin'
+gem 'rack-cors'
---
+++ app/controllers/api/v1/deal_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+    class DealController < BaseController
+      before_action :load_deal, only: %w(show)
+
+      def show
+        render json: @deal
+      end
+
+      private
+
+      def load_deal
+        @deal = Deal.find(params[:id])
+      end
+    end
+  end
+end
